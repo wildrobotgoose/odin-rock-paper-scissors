35f213e61bb82c5a53d0a11b68166e3ab358c211
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-07-30 10:51:24 -0400

    Implement getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const r = 3 * Math.random();
+    if (r < 1) {
+        return "rock";
+    }
+    else if (r < 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
